6382e2b4591b185d38a95ef0ff963a48ffa4abde
maks0000 <maksymbudzhak13@gmail.com>
2022-03-05 20:26:25 +0100

    Work with bottomNav,topAppBar,fragments,bottom sheet dialog fragment.
    Signed-off-by: maks0000 <maksymbudzhak13@gmail.com>

--- app/src/main/java/android/example/todo/MainActivity.kt
+++ app/src/main/java/android/example/todo/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private var binding: ActivityMainBinding? = null
 
@@ -14,34 +13,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding?.topAppBar)
 
 
 
-        supportFragmentManager.beginTransaction().replace(R.id.content, home()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
 
-        binding?.bottomNav?.setOnItemSelectedListener{ item ->
-            when(item.itemId) {
-                R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, home()).commit()
-                R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, shop()).commit()
-                R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, account()).commit()
-                R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, delivery  ()).commit()
+        binding?.bottomNav?.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Home()).commit()
+                R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Shop()).commit()
+                R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, account()).commit()
+                R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, delivery()).commit()
 
             }
-             true
+            true
         }
-      //  binding?.bottomNav?.selectedItemId = R.id.homeItemBottomNav
+         binding?.bottomNav?.selectedItemId = R.id.homeItemBottomNav
 
-        binding?.topAppBar?.setOnMenuItemClickListener { menuItem:MenuItem ->
-            when(menuItem.itemId) {
+        binding?.topAppBar?.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
                 R.id.favoritesTopNav -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.content,Favorites()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Favorites())
+                        .commit()
 
                     true
                 }
 
                 R.id.settingsTopNav -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.content,Settings()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Settings())
+                        .commit()
 
                     true
                 }
@@ -50,16 +57,39 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         val menuInflater = menuInflater
 
-        menuInflater.inflate(R.menu.top_menu,menu)
+        menuInflater.inflate(R.menu.top_menu, menu)
 
         return true
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            android.R.id.home -> {
+
+                val mainMenu = MainMenu()
+                mainMenu.show(
+                    supportFragmentManager,
+                    "main menu")
+
+            }
         }
-
-
+        return true
+    }
 }
+
+
+
+
+
+
+
+
+
